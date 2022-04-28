503ac36ee016c35de481e93bd3f6556f3356d31b
Jeongbin <yon980824@gmail.com>
2022-04-28 19:37:03 +0900

    TSX&CSS: Company

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,44 @@
-import React from 'react';
-import './App.css';
-import HTHeader from './HTHeader/HTHeader';
-import HTMain from './HTMain/HTMain';
-import HTCompany from './HTCompany/HTCompany';
-import HTHoneyStory from './HTHoneyStory/HTHoneyStory';
-import HTSweetNews from './HTSweetNews/HTSweetNews';
-import HTLink from './HTLink/HTLink';
-import HTFooter from './HTFooter/HTFooter';
+import React from "react";
+import "./App.css";
+import HTHeader from "./HTHeader/HTHeader";
+import HTMain from "./HTMain/HTMain";
+import HTCompany from "./HTCompany/HTCompany";
+import HTHoneyStory from "./HTHoneyStory/HTHoneyStory";
+import HTSweetNews from "./HTSweetNews/HTSweetNews";
+import HTLink from "./HTLink/HTLink";
+import HTFooter from "./HTFooter/HTFooter";
 
-function App() {
+// import "fullpage.js/vendors/scrolloverflow";
+// import "fullpage.js";
+// import "fullpage.js/dist/jquery.fullpage.min.css";
+// import $ from "jquery";
+
+function App(): JSX.Element {
+  // $((): void => {
+  //   ($("#fullpage") as any).fullpage({ scrollOverflow: true });
+  // });
   return (
     <div className="App">
       <header className="App-header">
-        <HTHeader/>
+        <HTHeader />
       </header>
-      <nav></nav>
-      <section>
-        <HTMain/>
-        <HTCompany/>
-        <HTHoneyStory/>
-        <HTSweetNews/>
-        <HTLink/>
+      <section className="section">
+        <HTMain />
+      </section>
+      <section className="section">
+        <HTCompany />
+      </section>
+      <section className="section">
+        <HTHoneyStory />
+      </section>
+      <section className="section">
+        <HTSweetNews />
+      </section>
+      <section className="section">
+        <HTLink />
       </section>
       <footer>
-        <HTFooter/>
+        <HTFooter />
       </footer>
     </div>
   );
--- src/HTCompany/HTCompany.tsx
+++ src/HTCompany/HTCompany.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
-import './HTCompany.css';
+import React from "react";
+import styles from "./HTCompany.module.css";
 
 function HTCompany() {
-    return (
-        <div>
-            <img src="images/company.png" alt="company" />
-        </div>
-    )
+  return (
+    <div className={styles.company}>
+      <div className={styles.caption}>
+        <h2>달콤한 세상으로의 초대</h2>
+        <p>해태제과는 늘 새롭고 달콤한 세상을 꿈꿉니다.</p>
+      </div>
+      <div className={styles.imgs}>
+        <img
+          className={styles.img01}
+          src="images/company/img-main0-0.png"
+          alt="company"
+        />
+        <img
+          className={styles.img02}
+          src="images/company/img-main0-1.png"
+          alt="company"
+        />
+        <img
+          className={styles.img03}
+          src="images/company/img-main0-2.png"
+          alt="company"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default HTCompany;
