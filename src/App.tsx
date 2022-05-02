c83498d9fe1f7baee419d720b504b9ae3bde157e
Jeongbin <yon980824@gmail.com>
2022-05-02 17:18:18 +0900

    Apply fullpage

--- src/App.tsx
+++ src/App.tsx
@@ -8,46 +8,40 @@ import HTSweetNews from "./HTSweetNews/HTSweetNews";
 import HTLink from "./HTLink/HTLink";
 import HTFooter from "./HTFooter/HTFooter";
 
-// import "fullpage.js/vendors/scrolloverflow";
-// import "fullpage.js";
-// import "fullpage.js/dist/jquery.fullpage.min.css";
-// import $ from "jquery";
+import $ from "jquery";
+import "fullpage.js/vendors/scrolloverflow";
+import "fullpage.js";
+import "fullpage.js/dist/jquery.fullpage.min.css";
 
 function App(): JSX.Element {
-  // $((): void => {
-  //   ($("#fullpage") as any).fullpage({ scrollOverflow: true });
-  // });
-  const [isMouseOver, setIsMouseOver] = useState(false);
-  useEffect(() => {}, [isMouseOver]);
+  $((): void => {
+    ($("#fullpage") as any).fullpage({ scrollOverflow: true });
+    console.log("hi");
+  });
+
   return (
     <div className="App">
-      <header
-        className="App-header"
-        onMouseEnter={() => {
-          setIsMouseOver(false);
-        }}
-        onMouseLeave={() => {
-          setIsMouseOver(false);
-        }}
-      >
+      <header className="App-header">
         <HTHeader />
       </header>
-      <section className="section">
-        <HTMain />
-      </section>
-      <section className="section">
-        <HTCompany />
-      </section>
-      <section className="section">
-        <HTHoneyStory />
-      </section>
-      <section className="section">
-        <HTSweetNews />
-      </section>
-      <section className="section">
-        <HTLink />
-      </section>
-      <footer>
+      <div id="fullpage">
+        <section className="section">
+          <HTMain />
+        </section>
+        <section className="section">
+          <HTCompany />
+        </section>
+        <section className="section">
+          <HTHoneyStory />
+        </section>
+        <section className="section">
+          <HTSweetNews />
+        </section>
+        <section className="section">
+          <HTLink />
+        </section>
+      </div>
+      <footer className="App-footer">
         <HTFooter />
       </footer>
     </div>
